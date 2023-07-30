59bdd19c9d7aad369eb6443c99fcab5cbb7dbd15
Diego Urrego <diegourrg@gmail.com>
2023-07-29 23:32:52 -0500

    I created a new DTO for updates and the respectivelies endpoints for update and delete a car.

--- src/cars/cars.service.ts
+++ src/cars/cars.service.ts
@@ -1,6 +1,7 @@
 import { Injectable, NotFoundException } from '@nestjs/common';
 import { Car } from './interfaces/car.interface';
 import { v4 as uuid } from 'uuid';
+import { CreateCarDto, UpdateCarDto } from './dto';
 
 /*
   ? Nota: los servicios es donde se agrega la lógica de negocio
@@ -33,5 +34,36 @@ export class CarsService {
     const car = this.cars.find((car) => car.id === id);
     if (!car) throw new NotFoundException(`car with id '${id}' not found`);
     return car;
+  }
+
+  create(createCarDto: CreateCarDto) {
+    const car: Car = {
+      id: uuid(),
+      ...createCarDto,
+    };
+    this.cars.push(car);
+    return car;
+  }
+
+  update(id: string, updateCarDto: UpdateCarDto) {
+    let carDB: Car = this.findOneById(id);
+    this.cars = this.cars.map((car: Car): Car => {
+      if (car.id === id) {
+        carDB = {
+          ...carDB,
+          ...updateCarDto,
+          id,
+        };
+        return carDB;
+      }
+      return car;
+    });
+    return carDB;
+  }
+
+  delete(id: string) {
+    const carDeleted = this.findOneById(id);
+    this.cars = this.cars.filter((car) => car.id !== id);
+    return carDeleted;
   }
 }
